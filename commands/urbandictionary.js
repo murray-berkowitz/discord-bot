c50fa91bdec9e498640b3e854e3b2a0ae0a2aab6
Murray Berkowitz <murray.berkowitz93@gmail.com>
2018-05-22 00:43:55 -0400

    updated vg command

--- commands/urbandictionary.js
+++ commands/urbandictionary.js
@@ -13,7 +13,7 @@ module.exports = {
       const embed = new Discord.RichEmbed()
       .setColor('#050517')
       .setAuthor('Urban Dictionary', 'https://slack-files2.s3-us-west-2.amazonaws.com/avatars/2018-01-11/297387706245_85899a44216ce1604c93_512.jpg')
-      .setDescription('Urban Dictionary is a crowdsourced online dictionary for slang words and phrases, operating under the motto "Define Your World.')
+      .setDescription('Urban Dictionary is a crowdsourced online dictionary for slang words and phrases, operating under the motto "Define Your World."')
       .addField("Word", res.word.charAt(0).toUpperCase() + res.word.slice(1), true)
       .addField('Definition', res.definition, true)
       .addField('Example(s)', res.example, true)
--- commands/vg.js
+++ commands/vg.js
@@ -1,4 +1,5 @@
 const {getGame} = require('../methods/vg')
+const Discord = require('discord.js')
 
 module.exports = {
   name: 'vg',
@@ -8,11 +9,23 @@ module.exports = {
   execute(message, args, command) {
     getGame(args.join(' '))
     .then(data => {
-      message.channel.send(`Game: ${data.name}\n\nSummary:\n${data.summary.split('').length > 1200 ? data.summary.slice(0, 1200) + '...' : data.summary}\n\n https://images.igdb.com/igdb/image/upload/t_cover_big/${data.cover.cloudinary_id}`)
-      //console.log(JSON.stringify(data, undefined,2))
+
+      // Maybe set data I want to display as their own variables, and if they dont exist display a "not found" text instead ie timeToBeatNormal = data.time_to_beat || "undefined"
+
+      const embed = new Discord.RichEmbed()
+      .setColor('#fa9529')
+      .setThumbnail(`https://images.igdb.com/igdb/image/upload/t_cover_big/${data.cover.cloudinary_id}`)
+      .setImage(`https://images.igdb.com/igdb/image/upload/t_screenshot_huge/${data.screenshots[~~(Math.random() * data.screenshots.length)].cloudinary_id}`)
+      .addField(data.name, data.summary.split('').length > 700 ? `${data.summary.slice(0, 700)}[ ...read more](${data.url})\n\n` : `${data.summary}\n\n`, true)
+      .addField('Time to Beat', `Normally: ${data.time_to_beat ? `${~~(data.time_to_beat.normally / 3600)}hrs` : 'Undefined' }      Completionist: ${data.time_to_beat ? `${~~(data.time_to_beat.completely / 3600)}hrs` : 'Undefined'}\n`)
+      .setTimestamp()
+      .setFooter("©IGDB", "https://images-na.ssl-images-amazon.com/images/I/51cz%2Bz-qDBL.png")
+
+      return message.channel.send(embed)
     })
-    .catch(err => message.channel.send(err))
+    .catch(err => console.log(err))
   }
 }
 
 //Artwork: https://images.igdb.com/igdb/image/upload/t_screenshot_huge/${data.artworks[0].cloudinary_id}
+//https://images.igdb.com/igdb/image/upload/t_cover_big/${data.cover.cloudinary_id}
